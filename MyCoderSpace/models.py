053fd81c7bc6826d6fede95e311636846ccdea2d
ajmarcano <aj.marcanobarrios@gmail.com>
2022-06-26 15:34:36 -0300

    agregado ckeditor

--- MyCoderSpace/models.py
+++ MyCoderSpace/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class BlogModel(models.Model):
     titulo = models.CharField(max_length=100)
     sub_titulo = models.CharField(max_length=100)
-    cuerpo = models.TextField()
-    imagen = models.URLField(max_length=300)
+    cuerpo = RichTextField(verbose_name="Contenido")
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_creacion = models.DateField(auto_now_add=True)
     
--- MyCoderSpace/urls.py
+++ MyCoderSpace/urls.py
@@ -3,12 +3,13 @@ from MyCoderSpace import views
 
 urlpatterns = [
     path("login/", views.BlogLogin.as_view(), name = "login"),
-    path("", views.BlogList.as_view(), name = "home"),
+    path("pages", views.BlogList.as_view(), name = "home"),
     path("create/", views.BlogCreate.as_view(), name = "create"),
     path("pages/<pk>/", views.BlogDetail.as_view(), name = "detail"),
     path("update/<pk>/", views.BlogUpdate.as_view(), name = "update"),
     path("delete/<pk>/", views.BlogDelete.as_view(), name = "delete"),
     path("logout", views.BlogLogout.as_view(), name='logout'),
-    path('About/', views.About, name="about")
+    path('About/', views.About, name="about"),
+    path('', views.Home, name="principal"),
 ]
 
