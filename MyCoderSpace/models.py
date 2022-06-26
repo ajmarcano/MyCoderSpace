26433ed87fad5581e4d71956836605bae1942638
ajmarcano <aj.marcanobarrios@gmail.com>
2022-06-26 18:43:30 -0300

    mensaje

--- MyCoderSpace/models.py
+++ MyCoderSpace/models.py
@@ -1,23 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 class BlogModel(models.Model):
     titulo = models.CharField(max_length=100)
     sub_titulo = models.CharField(max_length=100)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    cuerpo = RichTextUploadingField()
-=======
-    cuerpo = models.TextField()
     imagen = models.URLField(max_length=300)
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
-=======
->>>>>>> refs/remotes/origin/main
-=======
     cuerpo = RichTextField(verbose_name="Contenido")
->>>>>>> f6ca7e7598b410628912125949f7b288ba679a80
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_creacion = models.DateField(auto_now_add=True)
     imagen = models.URLField()
