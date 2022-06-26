68ad6801811e9730c5f3789636ac66766471bb6c
ajmarcano <aj.marcanobarrios@gmail.com>
2022-06-26 19:37:00 -0300

    arreglado codigo erroneo

--- blogger/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Avatar(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
